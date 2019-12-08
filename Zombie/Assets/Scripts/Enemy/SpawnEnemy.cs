16aa45b8ac0a70c1c81ff51745ecb19cf32c8e52
Fynn <fynn2301@outlook.de>
2019-12-09 00:05:25 +0100

    Added enemy spawner

---
+++ Zombie/Assets/Scripts/Enemy/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public float waitTime = 5f;
+    public GameObject spawnPoint;
+    public GameObject enemy1;
+    public bool waveStart = true;
+    // Start is called before the first frame update
+    float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > startTime + waitTime)
+        {
+            Instantiate(enemy1, spawnPoint.transform.position, spawnPoint.transform.rotation);
+            waitTime = 1f;
+            startTime = Time.time;
+        }
+    }
+}
